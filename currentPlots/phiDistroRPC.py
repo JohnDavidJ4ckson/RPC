7033be7fb200694d5ddf4c3e4860c2c32b1b8f6c
Daniel Navarro <aperez@fis.cinvestav.mx>
2018-10-19 03:44:12 +0200

    adding a fit to the lumicurrent vs lumi plots

--- currentPlots/phiDistroRPC.py
+++ currentPlots/phiDistroRPC.py
@@ -40,86 +40,14 @@ def rates_endcap_list(data):
     for row in reader:
       if not row: continue
       #print row
-      curry = float(row[1])
-      lummo = float(row[2])
+      curry = float(row[3])
+      lummo = float(row[4])
       current.append(curry)
       lumi.append(lummo)
   #print current
   #print lumi
 
   return [current, lumi]
-
-  rolls = ["_A","_B","_C"] #, "_D"]
-  chambers = [
-             "01","02","03","04","05","06","07","08","09",
-        "10","11","12","13","14","15","16","17","18","19",
-        "20","21","22","23","24","25","26","27","28","29",
-        "30","31","32","33","34","35","36"
-        ]
-  ring = ["_R2", "_R3"]
-  endcap = ["+4", "-4"]
-  names = []
-  rates = []
-  theDict = {}
-  for e in endcap:
-    for r in ring:
-      for roll in rolls:
-        #rates = []
-        for c in chambers:
-          try:
-            #print r
-            if float(rates1["rate"]["RE"+e+r+"_CH"+c+roll]["ratesquarecm"]) == 0: continue
-            rates.append(float(rates1["rate"]["RE"+e+r+"_CH"+c+roll]["ratesquarecm"]))
-            names.append("RE"+e+r+"_CH"+c+roll)
-            theDict["RE"+e+r+"_CH"+c+roll] = float(rates1["rate"]["RE"+e+r+"_CH"+c+roll]["ratesquarecm"])
-          except KeyError:
-            continue
-  #print sorted(names)
-  return [names, rates]
-  #return theDict
-
-## Null ->  List of lists [string, double]
-## The function uses a fixed txt file with the geometric information
-## of the RPC's in the endcaps and returns a list of names and their
-## corresponding pseudorapidity mean location.
-def phi_endcap_list():
-  lines = []
-  with open('endcapGeometryRPC.out', 'rw') as shakes:
-    for line in shakes:
-      y = line.split(",")
-      lines.append(y)
-  name = []
-  phiAvg = []
-  for line in lines:
-    nameList = line[0]
-    name.append(nameList.rstrip())
-    minPhiList = line[-2].split(" ")
-    minPhiValue = float( minPhiList[-1] )  
-    maxPhiList = line[-1].split(" ")
-    maxPhiValue = float( maxPhiList[-2] ) 
-    avgPhiValue = (minPhiValue + maxPhiValue) / 2
-    if avgPhiValue < 0: phiFromZero = avgPhiValue + 6.28318530718
-    else: phiFromZero = avgPhiValue 
-    phiDegrees = phiFromZero * (360 / 6.28318530718)
-    phiAvg.append(phiDegrees)
-  List = [name, phiAvg]
-  return List
-
-## List of lists [string, double], 
-## List of lists [string, double]  -> Dictionary
-## The function maps the rates and pseudorapidities that share ID 
-## names to each other and returns them in a dictionary
-def the_list(phiList, ratesList):
-  intersect = set(phiList[0]).intersection(ratesList[0])
-  #print len(intersect)
-  dict0 = {}
-  for i in intersect:
-    etaIndex = phiList[0].index(i)
-    rateIndex = ratesList[0].index(i)
-    indict = { "phi": phiList[1][etaIndex],
-               "rates":ratesList[1][rateIndex] }
-    dict0[i] = indict
-  return dict0
 
 ## array, array, string -> TGraph
 ## The function recieves an x and y values to create
@@ -140,51 +68,6 @@ def create_tgraphs(x, y, name):
   return gr
 
 ## dictionary -> dictionary
-## The function takes a dictionary that has the information about phi, rates, and the
-## Id/key of the RPC detector e.g. W+2_RB3-_S03_Forward
-## The function avarages over subrolls and rings such that the output key has the form
-## W+2_RB3_S03 and can be used for TGraph filling
-def granularity_average(dictionary):
-  rolls = ["_A","_B","_C"] #, "_D"]
-  chambers = [
-             "01","02","03","04","05","06","07","08","09",
-        "10","11","12","13","14","15","16","17","18","19",
-        "20","21","22","23","24","25","26","27","28","29",
-        "30","31","32","33","34","35","36"
-        ]
-  ring = ["_R2", "_R3"]
-  endcap = ["+4", "-4"]
-  dict0 = {}
-  for e in endcap:
-    for c in chambers: 
-      #Create lists to extract averages here
-      names = []
-      phi = []
-      rates = []
-      for k, v in dictionary.items():
-        for ri in ring:
-          for r in rolls:
-            if k == "RE"+e+ri+"_CH"+c+r: 
-              names.append(k)
-              cleanPhi = v["phi"]
-              rates.append( v["rates"] )
-              name = "RE"+e+"_CH"+c
-              if c == "19": cleanPhi = 180
-              if (c == "01" and cleanPhi > 300): cleanPhi -= 360
-              phi.append(cleanPhi)
-      try:
-        phiAvg   = sum(phi)/len(phi)
-        ratesAvg = sum(rates)/len(rates)
-        #print name, phiAvg, ratesAvg
-        #print "The phi list is ", phi
-        indict = { "phi":phiAvg,
-             "rates":ratesAvg }
-        dict0[name] = indict
-      except ZeroDivisionError:
-        continue
-  return dict0
-
-## dictionary -> dictionary
 ## This function takes the dictionary with the correct granularity and
 ## keys similar to W+2_RB3_S03. The chambers are used as the inputs of
 ## TGraphs. In the end a new dictionary is returned with a TGraphs 
@@ -198,38 +81,7 @@ def generate_tgraphs(dict0,name):
     xRE4.append(e)
   tgr  = create_tgraphs(xRE4,yRE4,name)
   print tgr
-
-
   return tgr 
-
-  chambers = [
-             "01","02","03","04","05","06","07","08","09",
-        "10","11","12","13","14","15","16","17","18","19",
-        "20","21","22","23","24","25","26","27","28","29",
-        "30","31","32","33","34","35","36"
-        ]
-  endcap = ["+4", "-4"]
-  
-  grDict = {}
-  for e in endcap:
-    #Create lists to average here
-    name = "RE"+e
-    xRB1, xRB2, xRB3, xRB4 = array('d'),array('d'),array('d'),array('d')
-    yRB1, yRB2, yRB3, yRB4 = array('d'),array('d'),array('d'),array('d')
-    xlist = [xRB1, xRB2, xRB3, xRB4]
-    ylist = [yRB1, yRB2, yRB3, yRB4]
-    for k,v in dict0.items():
-      for c in chambers:
-        if k == "RE"+e+"_CH"+c:
-          stationIndex = endcap.index(e)
-          #print stationIndex
-          xlist[stationIndex].append(v["phi"])
-          ylist[stationIndex].append(v["rates"])
-    tgr  = create_tgraphs(xlist[stationIndex],ylist[stationIndex],name)
-    #print name
-    #print tgr       
-    grDict[name] = tgr
-  return grDict
 
 ## list, string --> Nul
 ## The function receives a list with TGraphs for each RPC Layer
@@ -240,12 +92,12 @@ def plot_results(List, layer):
   #print "----- Creating Third TCanvas -----"
   c = TCanvas("c", "Canvas",W,H)
   ymax = 0.0
-  c.SetLeftMargin(0.15);
-  c.SetRightMargin(0.06);
-  c.SetTopMargin(0.09);
-  c.SetBottomMargin(0.14);
+  c.SetLeftMargin(0.15)
+  c.SetRightMargin(0.06)
+  c.SetTopMargin(0.09)
+  c.SetBottomMargin(0.14)
   gPad.SetGrid()
-  gPad.SetLogy()
+  #gPad.SetLogy()
   for gr in List:
     if ymax < max(gr.GetY()):
       ymax = max(gr.GetY())
@@ -259,7 +111,7 @@ def plot_results(List, layer):
   #print " ------------ Creating TMultiGraph -----------"
   List[0].SetMarkerColor( kBlue+2 )
   #List[0].SetMarkerStyle( 21 )
-  List[1].SetMarkerColor( kRed+2 )
+  List[1].SetMarkerColor( kGreen+3 )
   #List[1].SetMarkerStyle( 22 )
   #List[2].SetMarkerColor( kBlue )
   #List[3].SetMarkerStyle( 23 )
@@ -277,20 +129,24 @@ def plot_results(List, layer):
   #mg.Add(List[3],"AP")
   mg.Draw("a")
   mg.SetTitle( "")
-  mg.GetXaxis().SetTitle( 'Luminosity "online_luminosity_m1"' )
-  mg.GetYaxis().SetTitle( 'Current "imon"' )
-  mg.SetMaximum(ymax*3)
+  mg.GetXaxis().SetTitle( 'Instantaneous Luminosity (10^{30} cm^{-2} s^{-1})' )
+  mg.GetYaxis().SetTitle( 'Current (#mu A)' )
+  mg.SetMaximum(ymax+12)
+  #mg.SetMinimum(-10.0)
+  #mg.GetXaxis().SetLimits(0.,20000); 
   mg.GetXaxis().SetLabelFont(42)
   mg.GetXaxis().SetLabelOffset(0.007)
-  mg.GetXaxis().SetLabelSize(0.043)
+  mg.GetYaxis().CenterTitle(kTRUE)
+  mg.GetXaxis().SetNdivisions(507)
+  mg.GetXaxis().SetLabelSize(0.04)
   mg.GetXaxis().SetTitleSize(0.05)
-  mg.GetXaxis().SetTitleOffset(1.06)
+  mg.GetXaxis().SetTitleOffset(1.2)
   mg.GetXaxis().SetTitleFont(42)
   mg.GetYaxis().SetLabelFont(42)
-  mg.GetYaxis().SetLabelOffset(0.008)
+  mg.GetYaxis().SetLabelOffset(0.007)
   mg.GetYaxis().SetLabelSize(0.05)
   mg.GetYaxis().SetTitleSize(0.06)
-  mg.GetYaxis().SetTitleOffset(0.98)
+  mg.GetYaxis().SetTitleOffset(1.2)
   mg.GetYaxis().SetTitleFont(42)
 
   pv = TPaveText(.08,0.93,.45,0.93,"NDC") 
@@ -299,23 +155,33 @@ def plot_results(List, layer):
   pv.SetBorderSize(0)
   pv.SetTextSize(0.03)
   pv.Draw()
-  pv1 = TPaveText(.7,0.97,.9,0.97,"NDC")
-  #pv1.AddText('1.5 #times 10^{34} Hz/cm^{2} (13 TeV)')
+  pv1 = TPaveText(.55,0.93,.9,0.93,"NDC")
+  pv1.AddText('RE+4, CH09, CH10, CH11 (13 TeV)')
   pv1.SetFillStyle(0)
   pv1.SetBorderSize(0)
   pv1.SetTextSize(0.03)
   pv1.Draw()
-  l = TLegend(0.25,0.78,0.75,0.88)
+
+  fun1 = TF1("fun1","pol1",7000,19000)
+  fun2 = TF1("fun2","pol1",9000,19000)
+
+  fit1 = List[0].Fit("fun1","FQR") # "FQ")
+  fit2 = List[1].Fit("fun2","FQR") # "FQ")
+
+  offset1 = fun1.GetParameter(0)
+  offset2 = fun2.GetParameter(0)
+  slope1 = fun1.GetParameter(1)
+  slope2 = fun2.GetParameter(1)
+
+  l = TLegend(0.17,0.75,0.78,0.88)
   l.SetFillColor(0)
   l.SetBorderSize(0)
-  l.SetTextSize(0.03)
+  l.SetTextSize(0.025)
   l.SetNColumns(1)
-  l.AddEntry(List[0], "RE+4 runs from 2018.08.10 until 2018.09.10  (before TS2)", "p")
-  l.AddEntry(List[1], "RE+4 runs from 2018.09.15 until 2018.10.07  (after TS2)", "p")
-  #l.AddEntry(List[2], "RE-4 run 322355 (before TS2)", "p")
-  #l.AddEntry(List[3], "RE+4 run 322355 (before TS2)", "p")
-  #l.SetTextSize(0.05)
-  l.Draw("a");
+  l.AddEntry(List[0], "fill 7080 (before TS2) The offset is {0:.{1}f}".format(offset1,2) + " and the slope is {0:.{1}f}".format(slope1,4), "p")
+  l.AddEntry(List[1], "fill 7252 (after TS2) The offset is {0:.{1}f}".format(offset2,2)  + " and the slope is {0:.{1}f}".format(slope2,4), "p")
+  l.Draw("ap")
+
 
   c.SaveAs("currentDistroRPC{}.png".format(layer))
 
@@ -324,43 +190,19 @@ def plot_results(List, layer):
 ## in the agreed granularity then distribute it to the plotting function.
 def main():
   print "Retrieving rates Info"
-  listaRatesAfterTS2 =  rates_endcap_list("RE+4afterTS2/outputs/lhcfill_RE+4afterTS2.csv")
-  listaRatesBeforeTS2 = rates_endcap_list("RE+4beforeTS2/outputs/lhcfill_RE+4beforeTS2.csv")
-
+  print "Loading Files' Data"
+  listaRatesAfterTS2CH =  rates_endcap_list("RSafter3CH/outputs/lumisection_testAfterTS2Endcap.csv")
+  listaRatesBeforeTS2CH = rates_endcap_list("RSbefore3CH/outputs/lumisection_testBeforeTS2Endcap.csv")
   print "Generating TGraphs"
   print "THIS is AFTER"
-  tgraphsDictionaryAfterTS2  = generate_tgraphs(listaRatesAfterTS2,'RE+4 after TS2')
+  tgraphsDictionaryAfterTS2  = generate_tgraphs(listaRatesAfterTS2CH,'RE-4 after TS2')
   print "THIS is BEFORE"
-  tgraphsDictionaryBeforeTS2 = generate_tgraphs(listaRatesBeforeTS2,'RE+4 before TS2')
-
+  tgraphsDictionaryBeforeTS2 = generate_tgraphs(listaRatesBeforeTS2CH,'RE-4 before TS2')
   print "Creating plots"
   tgraphList = [ tgraphsDictionaryBeforeTS2, tgraphsDictionaryAfterTS2 ]
-  plot_results(tgraphList, "RE+4 Comparison")
+  plot_results(tgraphList, "RE+4_CH091011_TS2")
   print "DONE"
-
-
   return
-  print "Retrieving rates Info"
-  listaPhi = phi_endcap_list()
-  print "Creating phi-rates-id dictionary"
-  dictionary0AfterTS2  = the_list(listaPhi,listaRatesAfterTS2)
-  dictionary0BeforeTS2 = the_list(listaPhi,listaRatesBeforeTS2)
-  print "Taking the granularity average"
-  dictionary1AfterTS2  = granularity_average(dictionary0AfterTS2)
-  dictionary1BeforeTS2 = granularity_average(dictionary0BeforeTS2)
-  ##return dictionary1
-  print "Generating TGraphs"
-  tgraphsDictionaryAfterTS2  = generate_tgraphs(dictionary1AfterTS2)
-  tgraphsDictionaryBeforeTS2 = generate_tgraphs(dictionary1BeforeTS2)
-  print tgraphsDictionaryAfterTS2
-  print tgraphsDictionaryBeforeTS2
-  ##return tgraphsDictionary #Use this return for the ratio plotting script
-  print "Creating plots"
-  tgraphList = [ tgraphsDictionaryAfterTS2["RE-4"], tgraphsDictionaryAfterTS2["RE+4"], 
-                 tgraphsDictionaryBeforeTS2["RE-4"], tgraphsDictionaryBeforeTS2["RE+4"] ]
-  plot_results(tgraphList, "endcapComparison") 
-  print "DONE"
-  ##return tgraphsDictionary #Use this return for the ratio plotting script
 
 if __name__ == "__main__":
   main()
